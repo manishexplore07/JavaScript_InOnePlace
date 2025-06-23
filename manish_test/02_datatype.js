b5f036d24b8c0dcd076d3feff2104bacdc1b3fb8
Manish Kumar <manish.kumar.explore@gmail.com>
2025-06-23 14:09:40 +0000

    datatypes

---
+++ manish_test/02_datatype.js
@@ -0,0 +1,10 @@
+// number => 2 to power 53
+// bigint
+// string => IIII
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
