27725e432452b9b1d6986bdf243fe4dfe03f1a09
Manish Kumar <manish.kumar.explore@gmail.com>
2025-06-23 14:23:32 +0000

    Convrsion_datatype

---
+++ manish_test/03_conversionDatatype.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(boolean Is LoggedIn);
+// 1 => true; 0 => false
+// IIII => false
+// "hitesh" => true
+I
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
