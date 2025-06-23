3f4ed7553f1ef0b59caf812fec017faee9f77e43
Manish Kumar <manish.kumar.explore@gmail.com>
2025-06-23 09:00:51 +0000

    connection confiq done

---
+++ manish_test/test.js
@@ -0,0 +1,6 @@
+const accountId = 123456
+let accountEmail = "manish@gmail.com"
+var accountPassword = "123"
+accountCity = "Sasaram"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
