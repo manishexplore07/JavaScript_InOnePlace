7a85ef4692c77c1b304fc69e1d753e57ff51c0c4
Manish Kumar <manish.kumar.explore@gmail.com>
2025-06-27 19:33:45 +0000

    loop example

---
+++ manish_test/08_function_parameter.js
@@ -0,0 +1,6 @@
+function sum(first, second){
+    let add = first+second;
+    return add;
+}
+let result = sum(32,21);
+console.log(result);
---
+++ manish_test/09_while_loop.js
@@ -0,0 +1,6 @@
+let counter = 0;
+while(counter < 100){
+    console.log(`${counter+1}`)
+counter = counter + 1;
+}
+console.log("Ou of the loop")
