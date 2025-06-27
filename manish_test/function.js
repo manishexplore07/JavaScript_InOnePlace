2d23361bdc3fae34564aee530f7ae0aa75d86a62
Manish Kumar <manish.kumar.explore@gmail.com>
2025-06-27 19:04:09 +0000

    function and return statement

---
+++ manish_test/function.js
@@ -0,0 +1,17 @@
+function greeting(){
+    console.log("my name is manish kumar");
+}
+
+greeting();
+greeting();
+greeting();
+greeting();
+greeting();
+greeting();
+
+
+function getPrimeNumber(){
+    return 7;
+}
+let prime = getPrimeNumber();
+console.log(prime);
