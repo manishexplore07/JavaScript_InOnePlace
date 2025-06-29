f9340e4a9bda0c2f17f70838f9afabee852457b4
Manish Kumar <manish.kumar.explore@gmail.com>
2025-06-29 15:25:00 +0000

    fetching api

--- manish_test/advanced_javascript/fetch_api.js
+++ manish_test/advanced_javascript/fetch_api.js
@@ -13,6 +13,32 @@ It is a promise-based API, making it easier to handle
 asynchronous requests.*/
 
 
-
+/*
 fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
 .then(data => console.log(data));
+
+fetch('https://www.google.com').then(res => res.text())
+.then(data => console.log(data));
+
+
+async function fetchData() {
+let res = await fetch('https:www.google.com');
+let data = await res.text();
+console.log(data);
+}
+
+
+async function fetchData(url) {
+try {
+const response = await fetch (url);
+if (!response.ok) {
+throw new Error('Network response was not ok + response.statusText);
+};
+const data = await response.json();
+console.log(data);
+} catch (error) {
+console.log(error);
+}
+}
+fetchData('https://jsonplaceholder.typicode.com/posts');
+*/
